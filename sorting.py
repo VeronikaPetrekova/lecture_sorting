3c6276be1661c034ce0b5d220f9011e3ced8afc0
Veronika Petrekova <247290@vutbr.cz>
2023-04-17 10:05:28 +0200

    BUBBLE SORT

--- sorting.py
+++ sorting.py
@@ -34,10 +34,22 @@ def selection_sort(seznam):
         seznam[i], seznam[min_idx] = seznam[min_idx], seznam [i]
     return seznam
 
+def bubble_sort(seznam):
+    x = len(seznam)
+    for i in range(x-1):
+        for index in range(x-i-1):
+            if seznam[index] > seznam[index+1]:
+                seznam[index], seznam[index+1] = seznam[index+1], seznam[index]
+    return seznam
+
+
+
 def main():
     my_data = read_data("numbers.csv")
     zoradenie = selection_sort(my_data["series_3"].copy())
+    z = bubble_sort(my_data["series_3"].copy())
     print(my_data["series_3"])
+    print(z)
     print(zoradenie)
 
 if __name__ == '__main__':
