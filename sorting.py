4e73b9e9302850c7785757631a95e865c0818c75
Veronika Petrekova <247290@vutbr.cz>
2023-04-17 10:43:47 +0200

    INSERTION SORT

--- sorting.py
+++ sorting.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+
 def read_data(file_name):
     """
     Reads csv file and returns numeric data.
@@ -10,8 +11,8 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
-    with open(file_path, "r") as  csv_file:
-        reader = csv.DictReader (csv_file)
+    with open(file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
         data = {}
         for row in reader:
             for header, value in row.items():
@@ -23,16 +24,16 @@ def read_data(file_name):
     return data
 
 
-
 def selection_sort(seznam):
     x = len(seznam)
     for i in range(x):
         min_idx = i
-        for num_idx in range (i + 1, x):
+        for num_idx in range(i + 1, x):
             if seznam[num_idx] < seznam[min_idx]:
                 min_idx = num_idx
-        seznam[i], seznam[min_idx] = seznam[min_idx], seznam [i]
+        seznam[i], seznam[min_idx] = seznam[min_idx], seznam[i]
     return seznam
+
 
 def bubble_sort(seznam):
     x = len(seznam)
@@ -43,14 +44,24 @@ def bubble_sort(seznam):
     return seznam
 
 
+def insertion_sort(seznam_c):
+    x = len(seznam_c)
+    for i in range(1, x):
+        number = seznam_c[i]
+        k = i - 1
+        while k >= 0 and seznam_c[k] > number:
+            seznam_c[k+1] = seznam_c[k]
+            k = k - 1
+        seznam_c[k+1] = number
+    return seznam_c
+
 
 def main():
     my_data = read_data("numbers.csv")
-    zoradenie = selection_sort(my_data["series_3"].copy())
-    z = bubble_sort(my_data["series_3"].copy())
-    print(my_data["series_3"])
-    print(z)
-    print(zoradenie)
+    o = insertion_sort(my_data["series_2"].copy())
+    print(my_data["series_2"])
+    print(o)
+
 
 if __name__ == '__main__':
     main()
